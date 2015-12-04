ecb18838238f352e4990f7a975ca85dfde14fbc4
spatney <spatney@microsoft.com>
2015-12-04 02:47:24 -0800

    hello world stuff

--- setup.py
+++ setup.py
@@ -3,24 +3,28 @@
 
 import subprocess
 
-apt_packages = ['git', 'python-pip']
+apt_packages = ['git', 'python-pip', 'mongodb']
 pip_packages = ['flask', 'Flask-PyMongo']
 
 
-def install_apt():
-    cmd = ['sudo', 'apt-get', 'install','-y']
-    cmd.extend(apt_packages)
+def run_proc(cmd):
     p = subprocess.Popen(cmd)
     p.communicate()
+
+
+def install_apt():
+    cmd = ['sudo', 'apt-get', 'install', '-y']
+    cmd.extend(apt_packages)
+    run_proc(cmd)
 
 
 def install_pip():
     for pack in pip_packages:
         cmd = ['sudo', 'pip', 'install', pack]
-        cmd.extend(pip_packages)
-        p = subprocess.Popen(cmd)
-        p.communicate()
+        run_proc(cmd)
 
+run_proc(['sudo', 'apt-get', 'update'])
 install_apt()
 install_pip()
+
 
