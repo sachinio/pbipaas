947b98320d0b77e7987686d47b5ad815e49d8684
spatney <spatney@microsoft.com>
2015-12-04 01:53:00 -0800

    hello world stuff

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from __future__ import print_function
+#from __future__ import print_function
 
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import g
 
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+
 app = Flask(__name__)
+mongo = PyMongo(app)
 
+@app.route('/add/<name>')
+def add(name=None):
+    print mongo.db.user.insert_one({'name': name, 'power':'unknown power'})
+    return 'ok'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/hello/<name>')
+def index(name=None):
+    user = mongo.db.user.find_one_or_404({'name': name})
+    print user
+    return render_template('index.html', name=user['power'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
